cec0b45897fa8ea067cec9d24962cc0e4c7a7d4a
Lakshya_Bakshi <134377132+Lakshya-sketch@users.noreply.github.com>
2026-02-18 11:47:18 +0530

    TryCatch.java
    Throws vs Throw

--- TryCatch.java
+++ TryCatch.java
@@ -16,3 +16,26 @@ class Main {
         }
     }
 }
+
+class InvalidAgeException extends Exception{
+    InvalidAgeException(String message){
+        super(message);
+    }
+}
+class Main {
+    static void checkAge(int age) {
+        if(age < 18){
+            throw new InvalidAgeException("Age must be 18+");
+        }
+        System.out.println("Valid Age");
+    }
+    
+    public static void main(String[] args){
+        try{
+            checkAge(15);
+        }
+        catch(InvalidAgeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
