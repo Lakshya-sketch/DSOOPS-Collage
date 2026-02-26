188206ba1f41285b090ceeebd3e78ccf22d70616
Lakshya_Bakshi <134377132+Lakshya-sketch@users.noreply.github.com>
2026-02-26 12:00:53 +0530

    Add Student class with constructor and toString method
    Fixed constructor call for Student class and updated toString method.

--- ObjectReferences.java
+++ ObjectReferences.java
@@ -1,3 +1,5 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
 class Main {
     int roll;
@@ -10,6 +12,30 @@ class Main {
     
     public static void main(String[] args) {
         Main m1 = new Main();
+        System.out.print(m1);
+        Main m3 = new Main(r:101, n:"Lakshya");
+        Main m4 = new Main(r:101, n:"Lakshya");
+        
+        System.out.println(m3);
+        System.out.println(m4);
+    }
+}
+
+class Student{
+    int roll;
+    String name;
+    
+    Student(int r, String n){
+        roll = r;
+        name = n;
+    }
+    
+    public String toString(){
+        return roll+" "+name;
+    }
+    
+    public static void Student(String[] args) {
+        Main m1 = new Student();
         System.out.print(m1);
         Main m3 = new Main(r:101, n:"Lakshya");
         Main m4 = new Main(r:101, n:"Lakshya");
